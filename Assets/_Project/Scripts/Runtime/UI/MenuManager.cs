b63a59a7fdeab130ea1a0ebcd4487451883541d1
Kerwin Naidoo <kerwinjn18@gmail.com>
2023-11-29 15:38:56 +0200

    Main Menu Button Functionality, Custom Cursor and Scenes in Build

--- Assets/_Project/Scripts/Runtime/UI/MenuManager.cs
+++ Assets/_Project/Scripts/Runtime/UI/MenuManager.cs
@@ -1,15 +1,49 @@
+using Palmmedia.ReportGenerator.Core;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-    public class MenuManager : MonoBehaviour
+public class MenuManager : MonoBehaviour
+{
+    #region FIELDS
+    [SerializeField] private GameObject settingsPanel;
+    [SerializeField] private GameObject creditsPanel;
+
+    #endregion
+
+    #region UNITY METHODS
+    #endregion
+
+    #region METHODS
+    public void PlayButton()
     {
-        #region FIELDS
-        #endregion
-
-        #region UNITY METHODS
-        #endregion
-
-        #region METHODS
-        #endregion
+        SceneManager.LoadScene(1);
     }
+
+    public void ShowSettings()
+    {
+        settingsPanel.SetActive(true);
+    }
+
+    public void HideSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    public void ShowCredits()
+    {
+        creditsPanel.SetActive(true);
+    }
+
+    public void HideCredits()
+    {
+        creditsPanel.SetActive(false);
+    }
+
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+    #endregion
+}
